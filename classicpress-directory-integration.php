50647adce8b68669dd38369459e0ce7e9c6d1936
SIMONE FIORAVANTI <simospa@gieffeedizioni.it>
2024-04-02 11:23:27 +0200

    Change URL to production directory (#6)
    * Change URL
    * Bump version

--- classicpress-directory-integration.php
+++ classicpress-directory-integration.php
@@ -4,7 +4,7 @@
  * -----------------------------------------------------------------------------
  * Plugin Name:  ClassicPress Directory Integration
  * Description:  Install and update plugins from ClassicPress directory and keep ClassicPress themes updated.
- * Version:      0.1.0
+ * Version:      0.1.1
  * Author:       ClassicPress Contributors
  * Author URI:   https://www.classicpress.net
  * Plugin URI:   https://www.classicpress.net
