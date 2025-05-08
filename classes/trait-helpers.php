85b4572ab09682d164b235f110ec4775403c9ad0
Simone Fioravanti <simospa@gieffeedizioni.it>
2025-05-08 08:30:25 +0200

    Refactor code to comply with `wpcs` (#35)
    * Rename files
    
    * Apply phpcbf
    
    * Fix phpcs
    
    * Add wpcs workflow
    
    * Use ClassicPress standards
    
    * Add badges
    
    ---------
    
    Co-authored-by: xxsimoxx <simone@gieffeedizioni.it>

--- classes/trait-helpers.php
+++ classes/trait-helpers.php
@@ -2,8 +2,8 @@
 
 namespace ClassicPress\Directory;
 
-trait Helpers
-{
+trait Helpers {
+
 
 	/**
 	 * Get all substrings within text that are found between two other, specified strings
@@ -14,23 +14,22 @@ trait Helpers
 	 *
 	 * See https://stackoverflow.com/a/27078384
 	 */
-	function get_markdown_contents( $str, $startDelimiter, $endDelimiter ) {
-		$contents = [];
+	private function get_markdown_contents( $str, $startDelimiter, $endDelimiter ) {
+		$contents             = array();
 		$startDelimiterLength = strlen( $startDelimiter );
-		$endDelimiterLength = strlen( $endDelimiter );
-		$startFrom = $contentStart = $contentEnd = 0;
+		$endDelimiterLength   = strlen( $endDelimiter );
+		$startFrom            = $contentStart = $contentEnd = 0;
 
 		while ( $contentStart = strpos( $str, $startDelimiter, $startFrom ) ) { // phpcs:ignore WordPress.CodeAnalysis.AssignmentInCondition.FoundInWhileCondition
 			$contentStart += $startDelimiterLength;
-			$contentEnd = strpos( $str, $endDelimiter, $contentStart );
+			$contentEnd    = strpos( $str, $endDelimiter, $contentStart );
 			if ( $contentEnd === false ) {
 				break;
 			}
 			$contents[] = substr( $str, $contentStart, $contentEnd - $contentStart );
-			$startFrom = $contentEnd + $endDelimiterLength;
+			$startFrom  = $contentEnd + $endDelimiterLength;
 		}
 
 		return $contents;
 	}
-
 }
--- classicpress-directory-integration.php
+++ classicpress-directory-integration.php
@@ -24,28 +24,28 @@
 namespace ClassicPress\Directory;
 
 // Prevent direct access.
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
 // Bail if on WordPress
-if(!function_exists('classicpress_version')) {
-    add_action('admin_init', 'ClassicPress\Directory\deactivate_plugin_now');
-    add_action('admin_notices', 'ClassicPress\Directory\error_is_wp');
-    unset($_GET['activate']); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
-    return;
+if ( ! function_exists( 'classicpress_version' ) ) {
+	add_action( 'admin_init', 'ClassicPress\Directory\deactivate_plugin_now' );
+	add_action( 'admin_notices', 'ClassicPress\Directory\error_is_wp' );
+	unset( $_GET['activate'] ); // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+	return;
 }
 
 function deactivate_plugin_now() {
-    if (is_plugin_active('classicpress-directory-integration/classicpress-directory-integration.php')) {
-        deactivate_plugins('classicpress-directory-integration/classicpress-directory-integration.php');
-    }
+	if ( is_plugin_active( 'classicpress-directory-integration/classicpress-directory-integration.php' ) ) {
+		deactivate_plugins( 'classicpress-directory-integration/classicpress-directory-integration.php' );
+	}
 }
 
-function error_is_wp () {
-    $class = 'notice notice-error';
-    $message = __('ClassicPress Directory integration is a plugin meant to only work on ClassicPress sites.', 'classicpress-directory-integration');
-    printf('<div class="%1$s"><p>%2$s</p></div>', esc_attr($class), esc_html($message));
+function error_is_wp() {
+	$class   = 'notice notice-error';
+	$message = __( 'ClassicPress Directory integration is a plugin meant to only work on ClassicPress sites.', 'classicpress-directory-integration' );
+	printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
 }
 
 const DB_VERSION = 1;
@@ -55,32 +55,32 @@ require_once 'includes/constants.php';
 require_once 'includes/functions.php';
 
 // Load Helpers trait.
-require_once 'classes/Helpers.trait.php';
+require_once 'classes/trait-helpers.php';
 
 // Load Plugin Update functionality class.
-require_once 'classes/PluginUpdate.class.php';
+require_once 'classes/class-plugin-update.php';
 $plugin_update = new PluginUpdate();
 
 // Load Plugin Install functionality class.
-require_once 'classes/PluginInstall.class.php';
+require_once 'classes/class-plugin-install.php';
 $plugin_install = new PluginInstall();
 
 // Load Theme Update functionality class.
-require_once 'classes/ThemeUpdate.class.php';
+require_once 'classes/class-theme-update.php';
 $theme_update = new ThemeUpdate();
 
 // Load Theme Install functionality class.
-require_once 'classes/ThemeInstall.class.php';
+require_once 'classes/class-theme-install.php';
 $theme_install = new ThemeInstall();
 
 // Register text domain
 function register_text_domain() {
-	load_plugin_textdomain('classicpress-directory-integration', false, dirname(plugin_basename(__FILE__)).'/languages');
+	load_plugin_textdomain( 'classicpress-directory-integration', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
 }
-add_action('plugins_loaded', '\ClassicPress\Directory\register_text_domain');
+add_action( 'plugins_loaded', '\ClassicPress\Directory\register_text_domain' );
 
 // Add commands to WP-CLI
-require_once 'classes/WPCLI.class.php';
-if (defined('WP_CLI') && WP_CLI) {
-	\WP_CLI::add_command('cpdi', '\ClassicPress\Directory\CPDICLI');
+require_once 'classes/class-wpcli.php';
+if ( defined( 'WP_CLI' ) && WP_CLI ) {
+	\WP_CLI::add_command( 'cpdi', '\ClassicPress\Directory\CPDICLI' );
 }
